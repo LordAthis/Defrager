2e239a97e250741841256101cdeee486736b4634
LordAthis <lordathis@gmail.com>
2026-04-29 23:52:01 +0200

    Add scheduled task for automatic defragmentation

--- Scripts/Setup.ps1
+++ Scripts/Setup.ps1
@@ -5,3 +5,9 @@ if (!(Test-Path $WinScriptPath)) {
     Write-Log "[SIKER] Rendszer mappa letrehozva: $WinScriptPath"
 }
 # Ide masoljuk majd a szukseges scripteket a veglegesiteskor
+
+# Feladat letrehozasa a Windows-ban
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -WindowStyle Hidden -File `"$PSScriptRoot\Launcher.ps1`""
+$Trigger = New-ScheduledTaskTrigger -Monthly -DaysOfWeek Sunday -WeeksOfMonth First -At 3am
+$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+Register-ScheduledTask -TaskName "Defrager_AutoMaintenance" -Action $Action -Trigger $Trigger -Settings $Settings -Force
